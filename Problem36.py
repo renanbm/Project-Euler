baa2d55b71e4a6e0f20545d3d277b0091cb4c066
Renan Ben Moshe <renan.moshe@gmail.com>
2016-09-25 18:28:05 -0300

    Update Problem36.py

--- Problem36.py
+++ Problem36.py
@@ -3,10 +3,8 @@ def dec_to_bin(x):
 
 
 def palindromic(x):
-    x_str = str(x)
-    for i in range(len(x_str)):
-        if x_str[i] != x_str[len(x_str) - 1 - i]:
-            return False
+    if str(x) != str(x)[::-1]:
+        return False
     return True
 
 answer = 0
